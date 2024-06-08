530d4445bdc564256365e9cc8e5f29cece0b81e5
olinoles <oliver@noles.nz>
2024-06-08 21:36:39 +1200

    add shadows to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@ import { ReactComponent as StarSvg } from "../assets/star.svg";
 export function AppHeader() {
   return (
     <div className="flex justify-between items-center">
-      <div className="bg-white rounded-full py-0.5 flex items-center">
+      <div className="bg-white rounded-full py-0.5 flex items-center shadow-md">
         <div className="relative ml-3 flex items-center justify-center">
           <StarSvg className="mx-auto size-8" />
           <span className="absolute left-1/2 top-1/2 text-yellow-800 text-lg pt-1 -translate-y-1/2 -translate-x-1/2 font-semibold">
@@ -18,7 +18,7 @@ export function AppHeader() {
       <div>
         <img
           src="https://randomuser.me/api/portraits/men/1.jpg"
-          className="rounded-full size-10 border-white border-2"
+          className="rounded-full size-10 border-white border-2 shadow-md"
           alt="profile"
         />
       </div>
