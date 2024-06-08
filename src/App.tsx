4f164e0c3fdeae168ae822105e291900836325c7
olinoles <oliver@noles.nz>
2024-06-08 14:55:13 +1200

    add app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p>test font</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["poppins", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
