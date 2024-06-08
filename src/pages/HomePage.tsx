8c4287facb7534c01df2a4af1bc6155ddf76268d
olinoles <oliver@noles.nz>
2024-06-08 17:37:29 +1200

    layout fixes

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,13 +5,13 @@ import { PromoDeals } from "../components/promos";
 
 export function HomePage() {
   return (
-    <div className="max-w-lg gap-16 p-5 mx-auto relative h-full flex flex-1 flex-col">
+    <div className="max-w-lg gap-10 p-5 mx-auto relative h-full flex flex-1 flex-col">
       <AppHeader />
       <h1 className="text-white text-2xl mx-auto text-center">
         Good morning, <br />
         <b className="font-semibold">Oliver</b>
       </h1>
-      <div className="bg-white mt-24 border-2 flex flex-col gap-3 shadow-md p-3 rounded-[21px]">
+      <div className="bg-white mt-24 border-2 flex flex-col gap-6 shadow-md p-3 rounded-[21px]">
         {/* <DataAccessTest /> */}
         <LevelProgress />
         <h2 className="font-semibold text-sm text-black">Your promos</h2>
