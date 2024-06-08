45d92f699076186596048667610c22e8c7d07200
olinoles <oliver@noles.nz>
2024-06-09 08:53:36 +1200

    linting

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./App.css";
 
 import { HomeLayout } from "./pages/home-layout";
-import { PageLayout } from "./pages/page-layout";
 import { HomePage } from "./pages/HomePage";
 
 function App() {
