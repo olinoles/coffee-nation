ca54b8b6efb29758519204316c6ead090c0d4f5b
olinoles <oliver@noles.nz>
2024-06-09 15:35:34 +1200

    cafe page progress

--- src/pages/cafe-page.tsx
+++ src/pages/cafe-page.tsx
@@ -15,7 +15,7 @@ export function CafePage() {
       if (i < cafe.stamps_used) {
         stamps.push(
           <div
-            className="border-solid relative border border-brand-pink rounded-full p-1 w-[32px] h-[32px]"
+            className="border-solid relative border border-brand-pink rounded-full p-1  size-[40px]"
             key={i}
           >
             <CoffeeCupSvg className="size-5 absolute -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2" />
@@ -25,7 +25,7 @@ export function CafePage() {
       } else if (discount) {
         stamps.push(
           <div
-            className="border border-dashed flex justify-center rounded-full border-gray-500 w-[32px] h-[32px] items-center"
+            className="border border-dashed flex justify-center rounded-full border-gray-500  size-[40px] items-center"
             key={i}
           >
             <p className="text-black text-xs leading-0 -mt-2 text-center font-semibold">
@@ -37,7 +37,7 @@ export function CafePage() {
       } else if (i < cafe.stamps_total) {
         stamps.push(
           <div
-            className="border border-dashed rounded-full border-gray-500 w-[32px] h-[32px]"
+            className="border border-dashed rounded-full border-gray-500  size-[40px]"
             key={i}
           ></div>
         );
@@ -45,7 +45,7 @@ export function CafePage() {
       }
     }
     stamps.push(
-      <div className="border border-dashed rounded-full border-gray-500 w-[32px] h-[32px] flex justify-center items-center">
+      <div className="border border-dashed rounded-full border-gray-500  size-[40px] flex justify-center items-center">
         <p className="text-brand-pink text-xs font-semibold">FREE</p>
       </div>
     );
@@ -70,9 +70,25 @@ export function CafePage() {
       <h2 className="text-xs text-gray-400 font-medium">{cafe.address}</h2>
       <div className="flex flex-col mt-5 gap-5">
         <div className="flex gap-3 my-5">{getStamps(cafe)}</div>
-        <p className="text-sm font-medium text-gray-600">
-          Work in progress, more coming soon :)
-        </p>
+        <h1 className="text-base font-semibold">Prepaid - 10 Cup purchase</h1>
+        <div className="flex gap-3 flex-wrap">
+          <div className="border border-dashed rounded-full border-gray-500 size-[40px]"></div>
+          <div className="border border-dashed rounded-full border-gray-500 size-[40px]"></div>
+          <div className="border border-dashed rounded-full border-gray-500 size-[40px]"></div>
+          <div className="border border-dashed rounded-full border-gray-500 size-[40px]"></div>
+          <div className="border border-dashed rounded-full border-gray-500 size-[40px]"></div>
+          <div className="border border-dashed rounded-full border-gray-500 size-[40px]"></div>
+          <div className="border border-dashed rounded-full border-gray-500 size-[40px]"></div>
+          <div className="border border-dashed rounded-full border-gray-500 size-[40px]"></div>
+          <div className="border border-dashed rounded-full border-gray-500 size-[40px]"></div>
+          <div className="border border-dashed rounded-full border-gray-500 size-[40px]"></div>
+        </div>
+        <span className="text-xs text-gray-400 text-center font-medium">
+          PREPAY AND SAVE 35% ON EVERY CUP
+        </span>
+        <div className="bg-red-700 text-white rounded-xl p-2 py-3 font-semibold text-center">
+          BUY PREPAID CUPS
+        </div>
       </div>
     </div>
   ) : (
