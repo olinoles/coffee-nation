51735fe92b42c1db4685322224de59d681eb2a3b
olinoles <oliver@noles.nz>
2024-06-09 15:46:48 +1200

    add qr code

---
+++ src/components/qr-modal.tsx
@@ -0,0 +1,77 @@
+import QrCodeImage from "../assets/qrcode.png";
+import {
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+  Transition,
+  TransitionChild,
+} from "@headlessui/react";
+
+interface props {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+}
+
+export default function QrModal({ open, setOpen }: props) {
+  return (
+    <Transition show={open}>
+      <Dialog className="relative z-10" onClose={setOpen}>
+        <TransitionChild
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </TransitionChild>
+
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <TransitionChild
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <DialogPanel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+                <div>
+                  <img
+                    src={QrCodeImage}
+                    alt="QR Code"
+                    className="size-56 mx-auto"
+                  />
+                  <div className="mt-3 text-center sm:mt-5">
+                    <DialogTitle
+                      as="h3"
+                      className="text-base font-semibold leading-6 text-gray-900"
+                    >
+                      Your QR Code
+                    </DialogTitle>
+                    <div className="mt-2">
+                      <p className="text-sm text-gray-500">
+                        Use this code at the cafe to get your stamp.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="mt-5 sm:mt-6">
+                  <button
+                    type="button"
+                    className="inline-flex w-full justify-center rounded-md bg-red-700 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-700"
+                    onClick={() => setOpen(false)}
+                  >
+                    Return home
+                  </button>
+                </div>
+              </DialogPanel>
+            </TransitionChild>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
+  );
+}
--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,15 +1,19 @@
+import { useState } from "react";
 import CafeList from "../components/cafe-list";
 import { AppHeader } from "../components/header";
 import { LevelProgress } from "../components/level-progress";
 import { PromoDeals } from "../components/promos";
+import QrModal from "../components/qr-modal";
 
 export function HomePage() {
+  const [modalOpen, setModalOpen] = useState(false);
+
   return (
     <div className="max-w-lg gap-10 pb-20 p-5 mx-auto overflow-y-scroll relative h-full flex flex-1 flex-col">
       <AppHeader />
       <h1 className="text-white text-2xl mx-auto text-center">
         Good morning, <br />
-        <b className="font-semibold">Oliver</b>
+        <b className="font-semibold">James</b>
       </h1>
       <div className="bg-white mt-24 border-2 flex flex-col gap-4 shadow-md p-3 rounded-[21px]">
         {/* <DataAccessTest /> */}
@@ -18,7 +22,14 @@ export function HomePage() {
         <PromoDeals />
         <h2 className="font-semibold text-sm text-black">Your latest visits</h2>
         <CafeList />
+        <button
+          onClick={() => setModalOpen(true)}
+          className="bg-red-700 text-white rounded-xl p-2 py-3 font-semibold text-center"
+        >
+          Show QR Code
+        </button>
       </div>
+      <QrModal open={modalOpen} setOpen={setModalOpen} />
     </div>
   );
 }
